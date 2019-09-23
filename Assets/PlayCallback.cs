0639228b2fa85a1b1493e125c1175424dc448d2e
tranvande11091998 <tranvande19980911@gmail.com>
2019-09-23 12:05:39 +0700

    ok

---
+++ Assets/PlayCallback.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+
+using UnityEngine;
+
+public class PlayCallback : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position += new Vector3(Input.GetAxis("Mouse X"), 0, 0.5f);
+        Camera.main.transform.position += new Vector3(0, 0, 0.5f);         
+    }
+}
